b3f1f7c162d9fa1c631c27e16d63294d9dabda08
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2013-10-26 12:59:47 +0200

    Added autoclose parameter in prevision for https://github.com/inthepoche/poche/pull/290 to be merged

--- poche/poche.js
+++ poche/poche.js
@@ -9,7 +9,7 @@ function poche(id) {
 	    onComplete: function(transport) {
 		var ti = JSON.parse(transport.responseText);
 		
-		var share_url = ti.poche_url + "?action=add&url="+ btoa(ti.link);
+		var share_url = ti.poche_url + "?action=add&autoclose=true&url="+ btoa(ti.link);
 		console.log(share_url);
 		window.open(share_url,'_blank','menubar=no,height=390,width=600,toolbar=no,scrollbars=yes,status=no,dialog=1');
 		
